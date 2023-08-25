cd53b0fdcacea6943dedc1a753166731bbfe2e11
Arun Prakash Pandey <131158288+D32SSV@users.noreply.github.com>
2023-08-25 22:04:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function calculateMinCost() {
+function calculateMinCost(arr, n) {
   //your code here
-  
-  
-  
+	arr.sort();
+  let elementSum = [] ;
+	let sum = arr[0] ;
+	for(let i = 1; i<n; i++){
+		sum+= arr[i] ;
+		elementSum.push(sum);
+	}
+
+	result = 0;
+
+	for(let i in elementSum){
+		result += arr[i] ;
+	}
+	return result ;  
 }  
